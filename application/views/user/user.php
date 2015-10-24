456b356952cc10598abfb35c0a9443fb1083ae34
dimitris <dimkoug@gmail.com>
2015-10-24 09:53:28 +0300

    removed some files

--- application/views/user/user.php
+++ application/views/user/user.php
@@ -1,33 +1,23 @@
-
-
-
-    <div class="container">
-      <P>Εισάγετε την διέυθυνση σας και πατήσετε αναζήτηση μόλις εμφανιστεί στον χάρτη ο κύκλος σύρετε τον στον χάρτη για να δείτε τις στάσεις των λεωφορείων</P>
-      
-      <div class="row">
-        <div class="col-md-12">
-                  <form id="myForm"  class="form-inline input-small" role="form">
-    <input id="address" type="text" class="form-control" placeholder="Εισαγωγή Διεύθυνσης" />
-   <div class="form-group input-small">
- <button type="button"  id="bus" class="btn btn-primary">Αναζήτηση</button>
-</div>
-</form>
-
-
-
-        </div>
-
-
-
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div id="res"></div>
-           <div id="map" ></div>
-
-
-        </div>
-       
-
-      </div>
-    </div> 
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <center>
+        <form id="myForm" class="well form-inline">
+          <P>Εισάγετε την διέυθυνση σας και πατήσετε αναζήτηση μόλις εμφανιστεί στον χάρτη ο κύκλος σύρετε τον στον χάρτη για να δείτε τις στάσεις των λεωφορείων</P>
+          <div class="form-group">
+            <input id="address" type="text" class="form-control" placeholder="Εισαγωγή Διεύθυνσης" />
+          </div>
+          <div class="form-group input-small">
+              <button type="button"  id="bus" class="btn btn-primary">Αναζήτηση</button>
+          </div>
+        </form>
+      </center>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div id="res"></div>
+      <div id="map" ></div>
+    </div>
+  </div>
+</div> 
